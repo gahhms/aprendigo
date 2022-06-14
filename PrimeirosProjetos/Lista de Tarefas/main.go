c6b035178f5ebc9bb85fc080898a152c2ce326f1
Gabriel <viniga123@gmail.com>
2022-06-13 22:04:16 -0300

    Renomeando Pastas e mudanças na lista de tarefas

--- PrimeirosProjetos/Lista de Tarefas/main.go
+++ PrimeirosProjetos/Lista de Tarefas/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"sort"
 	"time"
 )
@@ -11,7 +13,7 @@ var funcao int
 type ToDoList struct {
 	ToDo  string
 	Date  time.Time
-	Added string
+	Added time.Time
 	Done  bool
 }
 
@@ -27,18 +29,21 @@ func statusToDo(t []ToDoList) {
 		bef := v.Date.Before(time.Now())
 		if v.Done == true {
 			status := "Feito\t"
-			arrumaData := v.Date.Format("02/01/2006")
-			fmt.Printf("%v\t%s\t%s\t\t%s\t\t%s", i, arrumaData, v.Added, status, v.ToDo)
+			arrumaDataTarefa := v.Date.Format("02/01/2006")
+			arrumaDataAdd := v.Added.Format("02/01/2006")
+			fmt.Printf("%v\t%s\t%s\t%s\t%s", i, arrumaDataTarefa, arrumaDataAdd, status, v.ToDo)
 			fmt.Println("")
 		} else if bef == true {
 			status := "Atrasado"
-			arrumaData := v.Date.Format("02/01/2006")
-			fmt.Printf("%v\t%s\t%s\t\t%s\t\t%s", i, arrumaData, v.Added, status, v.ToDo)
+			arrumaDataTarefa := v.Date.Format("02/01/2006")
+			arrumaDataAdd := v.Added.Format("02/01/2006")
+			fmt.Printf("%v\t%s\t%s\t%s\t%s", i, arrumaDataTarefa, arrumaDataAdd, status, v.ToDo)
 			fmt.Println("")
-		} else if v.Done == false { // ARRUMAR AQUI!!!!!!!!!!!!
+		} else if v.Done == false {
 			status := "Pendente"
-			arrumaData := v.Date.Format("02/01/2006")
-			fmt.Printf("%v\t%s\t%s\t\t%s\t\t%s", i, arrumaData, v.Added, status, v.ToDo)
+			arrumaDataTarefa := v.Date.Format("02/01/2006")
+			arrumaDataAdd := v.Added.Format("02/01/2006")
+			fmt.Printf("%v\t%s\t%s\t%s\t%s", i, arrumaDataTarefa, arrumaDataAdd, status, v.ToDo)
 			fmt.Println("")
 		}
 	}
@@ -50,13 +55,15 @@ func statusOnlyToDo(t []ToDoList) {
 		bef := v.Date.Before(time.Now())
 		if bef == true {
 			status := "Atrasado"
-			arrumaData := v.Date.Format("02/01/2006")
-			fmt.Printf("%v\t%s\t%s\t\t%s\t\t%s", i, arrumaData, v.Added, status, v.ToDo)
+			arrumaDataTarefa := v.Date.Format("02/01/2006")
+			arrumaDataAdd := v.Added.Format("02/01/2006")
+			fmt.Printf("%v\t%s\t%s\t%s\t%s", i, arrumaDataTarefa, arrumaDataAdd, status, v.ToDo)
 			fmt.Println("")
 		} else if v.Done == false {
 			status := "Pendente"
-			arrumaData := v.Date.Format("02/01/2006")
-			fmt.Printf("%v\t%s\t%s\t\t%s\t\t%s", i, arrumaData, v.Added, status, v.ToDo)
+			arrumaDataTarefa := v.Date.Format("02/01/2006")
+			arrumaDataAdd := v.Added.Format("02/01/2006")
+			fmt.Printf("%v\t%s\t%s\t%s\t%s", i, arrumaDataTarefa, arrumaDataAdd, status, v.ToDo)
 			fmt.Println("")
 		}
 	}
@@ -89,7 +96,8 @@ func showToDo(t []ToDoList) {
 func escolhaFuncao(i int) {
 	switch i {
 	case 1:
-		fmt.Println("one")
+		addTarefa()
+		voltarMenu()
 	case 2:
 		show(Tarefas)
 		voltarMenu()
@@ -104,9 +112,31 @@ func escolhaFuncao(i int) {
 	case 5:
 		fmt.Println("Fim do Programa!")
 	default:
-		fmt.Printf("\nOpção Inválida! Digitou errado?\n\n")
+		fmt.Printf("Opção Inválida! Digitou errado?\n\n")
 		lerEscolha()
 	}
+}
+
+func addTarefa() {
+	fmt.Printf("Descrição da tarefa: ")
+	descricao1 := lerDescricao()
+
+	fmt.Printf("Data da entrega da tarefa: ")
+	var dataNovaTarefa string
+	_, err1 := fmt.Scan(&dataNovaTarefa)
+	if err1 != nil {
+		fmt.Println("Deu xabu, tenta de novo!")
+		fmt.Println(err1)
+	}
+	dataNovaTarefaTime, err := time.Parse("02/01/2006", dataNovaTarefa)
+	if err != nil {
+		fmt.Println("Deu xabu, tenta de novo!")
+		fmt.Println(err)
+	}
+	adicao := []ToDoList{{descricao1, dataNovaTarefaTime, time.Now(), false}}
+	Tarefas = append(Tarefas, adicao...)
+	fmt.Println("Tarefa adicionada com sucesso!")
+	show(Tarefas)
 }
 
 func lerEscolha() {
@@ -114,9 +144,10 @@ func lerEscolha() {
 	fmt.Printf("Escolha a função que gostaria de executar!")
 	fmt.Printf("\n1 - Adicionar nova tarefa\n2 - Listar todas as tarefas\n3 - Listar tarefas por data\n4 - Listar tarefas pendentes\n5 - Encerrar programa\n")
 	fmt.Printf("Sua escolha: ")
-	_, err := fmt.Scanln(&funcao)
+	_, err := fmt.Scan(&funcao)
 	if err != nil {
 		fmt.Println("Deu xabu, tenta de novo!")
+
 	}
 	escolhaFuncao(funcao)
 }
@@ -126,9 +157,10 @@ func voltarMenu() {
 	var voltamenu string
 	fmt.Println("")
 	fmt.Printf("Deseja voltar ao Menu principal? (s/n): ")
-	_, err := fmt.Scanln(&voltamenu)
+	_, err := fmt.Scan(&voltamenu)
 	if err != nil {
 		fmt.Println("Deu xabu, tenta de novo!")
+
 	}
 	switch voltamenu {
 	case "s":
@@ -143,9 +175,21 @@ func voltarMenu() {
 }
 
 var Tarefas = []ToDoList{
-	{"Lavar roupas brancas", time.Date(2023, time.June, 15, 23, 0, 0, 0, time.UTC), "03/06/2022", false},
-	{"Escrever o código dessa lista ", time.Date(2009, time.January, 07, 23, 0, 0, 0, time.UTC), "02/06/2022", false},
-	{"Levar o cachorro para cagar", time.Date(2022, time.June, 01, 23, 0, 0, 0, time.UTC), "02/06/2022", false},
+	{"Lavar roupas brancas", time.Date(2023, time.June, 15, 23, 0, 0, 0, time.UTC), time.Date(2023, time.June, 15, 23, 0, 0, 0, time.UTC), false},
+	{"Escrever o código dessa lista ", time.Date(2009, time.January, 07, 23, 0, 0, 0, time.UTC), time.Date(2023, time.June, 15, 23, 0, 0, 0, time.UTC), false},
+	{"Levar o cachorro para cagar", time.Date(2022, time.June, 01, 23, 0, 0, 0, time.UTC), time.Date(2023, time.June, 15, 23, 0, 0, 0, time.UTC), false},
+}
+
+func lerDescricao() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	descricao := scanner.Text()
+	fmt.Printf(descricao)
+	scanner1 := bufio.NewScanner(os.Stdin)
+	scanner1.Scan()
+	descricao1 := scanner1.Text()
+	return descricao1
+
 }
 
 func main() {
